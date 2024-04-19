baa9e7a616f4ade5b25bf72e5689cf3b74e60664
LeonKohli <98176333+LeonKohli@users.noreply.github.com>
2024-04-20 00:51:28 +0200

    working with codepen

--- content.js
+++ content.js
@@ -3,15 +3,17 @@ setInterval(() => {
     const messageContainer = htmlCodeSpace.closest("[data-message-id]");
     if (!messageContainer) return;
 
-    let iframe = htmlCodeSpace.querySelector("iframe");
+    let iframe = messageContainer.querySelector("iframe");
 
     if (!iframe) {
       iframe = document.createElement("iframe");
-      htmlCodeSpace.parentElement.style.padding = "0";
       iframe.style.width = "100%";
-      iframe.style.height = "60vh";
+      iframe.style.height = "400px";
+      iframe.style.border = "none";
+      iframe.style.borderRadius = "5px";
+      iframe.style.boxShadow = "0 2px 4px rgba(0, 0, 0, 0.1)";
       iframe.setAttribute("sandbox", "allow-same-origin allow-scripts");
-      htmlCodeSpace.appendChild(iframe);
+      messageContainer.appendChild(iframe);
     }
 
     const htmlContent = htmlCodeSpace.innerText;
@@ -25,6 +27,30 @@ setInterval(() => {
 
     if (iframe.srcdoc !== newContent) {
       iframe.srcdoc = newContent;
+    }
+
+    // Create "Edit on CodePen" button
+    let codepenButton = messageContainer.querySelector(".codepen-button");
+    if (!codepenButton) {
+      const data = {
+        html: htmlContent,
+        css: Array.from(messageContainer.querySelectorAll(".language-css"))
+          .map((cssCodeSpace) => cssCodeSpace.innerText)
+          .join(""),
+      };
+
+      const JSONstring = JSON.stringify(data)
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&apos;");
+
+      const form = `
+        <form action="https://codepen.io/pen/define" method="POST" target="_blank" class="codepen-button">
+          <input type="hidden" name="data" value='${JSONstring}'>
+          <input type="submit" value="Edit on CodePen">
+        </form>
+      `;
+
+      messageContainer.insertAdjacentHTML("beforeend", form);
     }
   });
 }, 2000);
