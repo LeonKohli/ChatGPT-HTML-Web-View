5f3293a4304f53d7ca1beeb55ec73b5f9cbb490f
Leon Kohli <98176333+LeonKohli@users.noreply.github.com>
2024-08-20 14:17:52 +0200

    feat: changed to individual files

---
+++ promo/script.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.faq-question').forEach(question => {
+  question.addEventListener('click', () => {
+    const faqItem = question.parentElement;
+    faqItem.classList.toggle('active');
+  });
+});
+
+const sections = document.querySelectorAll('section');
+
+const observerOptions = {
+  root: null,
+  threshold: 0.1,
+};
+
+const observerCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate');
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
